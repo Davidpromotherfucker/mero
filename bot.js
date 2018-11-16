43736e8f302461a0783f907e1ac021e9bb2689e2
Davidpromotherfucker <44382470+Davidpromotherfucker@users.noreply.github.com>
2018-11-16 13:51:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "c";
-var adminprefix = 'c'
+var prefix = "N";
+var adminprefix = 'N'
 
-const developers = ["472413769700474901","id"]
+const developers = ["477591209943105538"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
